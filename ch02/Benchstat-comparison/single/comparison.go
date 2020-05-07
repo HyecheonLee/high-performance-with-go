be2521341db19f8973653cff9c937e58558c7f0d
hyecheon <rainbow880616@gmail.com>
2020-05-08 01:10:36 +0900

    벤치마킹

---
+++ ch02/Benchstat-comparison/single/comparison.go
@@ -0,0 +1,22 @@
+package single
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type CarData struct {
+	Cylinders int
+	Brand     string
+	Mpg       float64
+}
+
+func CarJSON() {
+	honda := CarData{Cylinders: 4, Brand: "Toyota", Mpg: 42.6}
+	carDataJson, err := json.Marshal(honda)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Sprintf(string(carDataJson))
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
-module "hyecheonlee/high-performance-with-go"
+module hyecheonlee/high-performance-with-go
+
+go 1.14
+
+require golang.org/x/perf v0.0.0-20200318175901-9c9101da8316 // indirect
